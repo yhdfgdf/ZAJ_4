126c4fb90f2133e8bd1d67f4bb3204839b12aceb
s31078 <s31078@pjwstk.edu.pl>
2024-05-05 18:43:09 +0200

    leć

--- Multiplier.java
+++ Multiplier.java
@@ -1,2 +1,19 @@
 public class Multiplier {
+    int x,y;
+    double a,b;
+    public void multiplier(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+
+    public void {
+
+    }
+
+    public void multiplier(double a, double b){
+        this.a=a;
+        this.b=b;
+    }
+
+
 }
